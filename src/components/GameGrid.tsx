b378de6a7428d4822ef2f3598161ff56495b97c0
malt9830 <malt9830@stud.kea.dk>
2024-02-06 20:22:06 +0100

    Added initial grid layout

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,3 +1,23 @@
 export default function GameGrid() {
-  return <div></div>
+  return (
+    <div className="grid grid-cols-5 grid-rows-5">
+      <div>x</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>x</div>
+
+      <div>1</div>
+
+      <div className="col-span-3 row-span-3">o</div>
+
+      <div>x</div>
+
+      <div>2</div>
+      <div>x</div>
+
+      <div>3</div>
+      <div>x</div>
+    </div>
+  )
 }
