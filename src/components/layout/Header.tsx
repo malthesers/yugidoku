e488cebdad62392fe07ce307904860ffd6174292
malt9830 <malt9830@stud.kea.dk>
2024-02-06 21:08:06 +0100

    Pseudo-styled header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
-    <header>
-      <p>yugidoku</p>
+    <header className="bg-zinc-900">
+      <div className="max-w-2xl flex flex-row justify-between mx-auto px-6 py-4">
+        <p>Yu-Gi-Doku!</p>
+        <nav>x</nav>
+      </div>
     </header>
   )
 }
