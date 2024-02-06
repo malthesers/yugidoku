0813e1d8189156fd41f35083ef70df3c30965f42
malt9830 <malt9830@stud.kea.dk>
2024-02-06 21:05:37 +0100

    Changed labels to be based on level object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+'use client'
+
 import GameGrid from '@/components/GameGrid'
 import LabelBox from '@/components/LabelBox'
+import { Matrix } from '@/types'
+import { useState } from 'react'
 
 export default function Home() {
+  const [level, setLevel] = useState<Matrix>({
+    column1: 'Water',
+    column2: 'Level 4',
+    column3: 'ATK 1500',
+    row1: 'Pyro',
+    row2: 'Fusion',
+    row3: 'Fish'
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="grid grid-cols-5 grid-rows-5 aspect-square max-w-xl w-full">
+      <div className="grid grid-cols-5 grid-rows-5 aspect-square max-w-2xl w-full">
         <LabelBox label="x" />
-        <LabelBox label="WATER" />
-        <LabelBox label="Level 4" />
-        <LabelBox label="ATK 1500" />
+        <LabelBox label={level.column1} />
+        <LabelBox label={level.column2} />
+        <LabelBox label={level.column3} />
         <LabelBox label="x" />
-        <LabelBox label="Pyro" />
+        <LabelBox label={level.row1} />
         <GameGrid />
         <LabelBox label="x" />
-        <LabelBox label="Effect" />
+        <LabelBox label={level.row2} />
         <LabelBox label="x" />
-        <LabelBox label="Fish" />
+        <LabelBox label={level.row3} />
         <LabelBox label="x" />
         <div className="col-span-5 grid place-content-center">
           <p className="text-center">bottom text</p>
