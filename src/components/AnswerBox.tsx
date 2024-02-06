6866e2d6d6ad63d1c9cb3bc5a4103d465cf077cf
malt9830 <malt9830@stud.kea.dk>
2024-02-06 20:53:16 +0100

    Added initial GameGrid styling and border radius

--- src/components/AnswerBox.tsx
+++ src/components/AnswerBox.tsx
@@ -1,10 +1,11 @@
 interface Props {
   answer: string
+  className?: string
 }
 
-export default function AnswerBox({ answer }: Props) {
+export default function AnswerBox({ answer, className }: Props) {
   return (
-    <div className="size-full grid place-content-center">
+    <div className={className + ' size-full grid place-content-center bg-zinc-900'}>
       <p>{answer}</p>
     </div>
   )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,16 +2,16 @@ import AnswerBox from './AnswerBox'
 
 export default function GameGrid() {
   return (
-    <div className="grid grid-cols-3 grid-rows-3 col-span-3 row-span-3">
+    <div className="grid grid-cols-3 grid-rows-3 col-span-3 row-span-3 gap-2">
+      <AnswerBox answer="test" className="rounded-tl-3xl" />
+      <AnswerBox answer="test" />
+      <AnswerBox answer="test" className="rounded-tr-3xl" />
       <AnswerBox answer="test" />
       <AnswerBox answer="test" />
       <AnswerBox answer="test" />
+      <AnswerBox answer="test" className="rounded-bl-3xl" />
       <AnswerBox answer="test" />
-      <AnswerBox answer="test" />
-      <AnswerBox answer="test" />
-      <AnswerBox answer="test" />
-      <AnswerBox answer="test" />
-      <AnswerBox answer="test" />
+      <AnswerBox answer="test" className="rounded-br-3xl" />
     </div>
   )
 }
