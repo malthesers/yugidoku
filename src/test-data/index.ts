3f49478352f82dfaf21d864475e8d3d5fdfc2f6d
malt9830 <malt9830@stud.kea.dk>
2024-02-07 15:21:27 +0100

    Added 3 test cards

---
+++ src/test-data/index.ts
@@ -0,0 +1,35 @@
+export const monsters = [
+  {
+    id: 6983839,
+    name: 'Tornado Dragon',
+    cardType: 'XYZ Monster',
+    desc: '2 Level 4 monsters\nOnce per turn (Quick Effect): You can detach 1 material from this card, then target 1 Spell/Trap on the field; destroy it.',
+    atk: 2100,
+    def: 2000,
+    level: 4,
+    type: 'Wyrm',
+    attribute: 'WIND'
+  },
+  {
+    id: 40916023,
+    name: 'Aqua Spirit',
+    cardType: 'Effect Monster',
+    desc: "Cannot be Normal Summoned/Set. Must first be Special Summoned (from your hand) by banishing 1 WATER monster from your GY. Once per turn, during your opponent's Standby Phase: You can target 1 face-up monster your opponent controls; change that target's battle position, and if you do, it cannot change its battle position for the rest of this turn.",
+    atk: 1600,
+    def: 1200,
+    level: 4,
+    type: 'Aqua',
+    attribute: 'WATER'
+  },
+  {
+    id: 94145021,
+    name: 'Droll & Lock Bird',
+    cardType: 'Effect Monster',
+    desc: "If a card(s) is added from the Main Deck to your opponent's hand, except during the Draw Phase (Quick Effect): You can send this card from your hand to the GY; for the rest of this turn, cards cannot be added from either player's Main Deck to the hand.",
+    atk: 0,
+    def: 0,
+    level: 1,
+    type: 'Spellcaster',
+    attribute: 'WIND'
+  }
+]
