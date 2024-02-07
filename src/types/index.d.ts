8702b61f813a5ea75c35569ce1ae80614beadf6f
malt9830 <malt9830@stud.kea.dk>
2024-02-07 13:06:30 +0100

    Updated types to include Level and Attribute

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,3 +6,15 @@ export interface Matrix {
   row2: string
   row3: string
 }
+
+type Property = Level | Attribute
+
+interface Level extends Property {
+  property: 'level'
+  value: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+}
+
+interface Attribute extends Property {
+  property: 'attribute'
+  value: 'DARK' | 'LIGHT' | 'EARTH' | 'WIND' | 'WATER' | 'FIRE'
+}
