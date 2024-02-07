55be062f4f1cd2598c723cba6d24b23d99bfc78d
malt9830 <malt9830@stud.kea.dk>
2024-02-07 14:21:33 +0100

    Added types for Type and Card Type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,14 +7,54 @@ export interface Matrix {
   row3: Property
 }
 
-type Property = Level | Attribute
+type Property = Level | Attribute | Type
 
-interface Level extends Property {
+interface BaseProperty {
+  property: string
+  value: string | number
+}
+
+interface Level extends BaseProperty {
   property: 'Level'
   value: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 }
 
-interface Attribute extends Property {
+interface Attribute extends BaseProperty {
   property: 'Attribute'
   value: 'DARK' | 'LIGHT' | 'EARTH' | 'WIND' | 'WATER' | 'FIRE'
+}
+
+interface Type extends BaseProperty {
+  property: 'Type'
+  value:
+    | 'Aqua'
+    | 'Beast'
+    | 'Beast-Warrior'
+    | 'Cyberse'
+    | 'Dinosaur'
+    | 'Divine-Beast'
+    | 'Dragon'
+    | 'Fairy'
+    | 'Fiend'
+    | 'Fish'
+    | 'Insect'
+    | 'Illusion'
+    | 'Machine'
+    | 'Plant'
+    | 'Psychic'
+    | 'Pyro'
+    | 'Reptile'
+    | 'Rock'
+    | 'Sea Serpent'
+    | 'Spellcaster'
+    | 'Thunder'
+    | 'Warrior'
+    | 'Winged Beast'
+    | 'Wyrm'
+    | 'Zombie'
+}
+
+interface CardType extends BaseProperty {
+  property: 'Card Type'
+  value: 'Normal' | 'Effect' | 'Ritual' | 'Fusion' | 'Synchro' | 'Xyz' | 'Pendulum' | 'Link'
 }
